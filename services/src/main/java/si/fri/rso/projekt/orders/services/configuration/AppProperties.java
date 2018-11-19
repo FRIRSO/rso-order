6888fd9ef9f2c1e6c47ffcff1f7566f082852828
krneElL <susnikg@gmail.com>
2018-11-19 17:32:03 +0100

    init

--- services/src/main/java/si/fri/rso/projekt/orders/services/configuration/AppProperties.java
+++ services/src/main/java/si/fri/rso/projekt/orders/services/configuration/AppProperties.java
@@ -1,10 +1,15 @@
 package si.fri.rso.projekt.orders.services.configuration;
 
 import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+import com.kumuluz.ee.configuration.cdi.ConfigValue;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 @ConfigBundle("app-properties")
 public class AppProperties {
+
+    @ConfigValue(value = "external-services.enabled", watch = true)
+    private boolean externalServicesEnabled;
+
 }
