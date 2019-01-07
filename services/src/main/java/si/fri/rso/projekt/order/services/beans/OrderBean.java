2953e8e15932e4374d70082b6462368378d2ec76
krneElL <susnikg@gmail.com>
2019-01-07 10:01:44 +0100

    Log fix

--- services/src/main/java/si/fri/rso/projekt/order/services/beans/OrderBean.java
+++ services/src/main/java/si/fri/rso/projekt/order/services/beans/OrderBean.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
